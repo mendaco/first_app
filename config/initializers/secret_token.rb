08d6a0127857a2f864652933ad3126f5a3119578
kiuchi <mendaco3104@gmail.com>
2014-04-02 14:38:08 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sample::Application.config.secret_key_base = 'f10ae6d7be8faf27f916ced745bde6b453e044850a58b229a0f949b061ee72fbef649933f6860121e33b04ac0f85c813592b3aca62c71059363c269dfcdc6f23'
---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/generator/siteconf20140401-40749-mvsii9.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140401-40749-pjslcx"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
